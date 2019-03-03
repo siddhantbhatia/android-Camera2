b9436f1be7c2a26dc9699927684f57cdb19eca1f
Siddhant <sid.rac125@gmail.com>
2019-03-03 17:41:37 +0800

    Initial commit

---
+++ app/src/main/java/com/siddhantbhatia/camera2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.siddhantbhatia.camera2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button openCameraBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
